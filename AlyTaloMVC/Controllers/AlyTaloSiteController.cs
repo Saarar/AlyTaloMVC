c103dcef5ef6c138757ab2616ae0e663028c3074
Saaramaria <saaramaria.rassa@student.careeria.fi>
2019-12-03 12:47:15 +0200

    Lisätty luokat, controllerit ja näkymät

---
+++ AlyTaloMVC/Controllers/AlyTaloSiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlyTaloMVC.Controllers
+{
+    public class AlyTaloSiteController : Controller
+    {
+        // GET: AlyTaloSite
+        public ActionResult HomeSite()
+        {
+            return View();
+        }
+    }
+}
